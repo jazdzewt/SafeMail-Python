ea1342f06135f42673418a7625e6f68d2d8b262b
jazdzewt <jazdzewskitymon@gmail.com>
2026-01-12 17:54:00 +0100

    Zmiana app.py.

--- web/app.py
+++ web/app.py
@@ -1,18 +1,23 @@
 from flask import Flask
 import os
 
-def get_secret(secret_name, default=None):
+def get_secret(secret_name):
     # 1. Najpierw próbujemy Docker Secrets (plik)
     try:
         with open(f'/run/secrets/{secret_name}', 'r') as file:
             return file.read().strip()
     except IOError:
         # 2. Jak nie ma pliku, szukamy w zmiennych (dla kompatybilności)
-        return os.environ.get(secret_name.upper(), default)
+        key = os.environ.get(secret_name.upper())
+        if key:
+            return key
+            
+    # 3. Jeśli nigdzie nie ma klucza - STOP! Nie uruchamiaj aplikacji
+    raise ValueError(f"CRITICAL ERROR: No Secret '{secret_name}'!")
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = get_secret('flask_key_file', 'klucz_zapasowy_dev')
+app.config['SECRET_KEY'] = get_secret('flask_key_file')
 
 @app.route('/')
 def hello():
