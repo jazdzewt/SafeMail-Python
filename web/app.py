842ef6c033b177172c32b5ef6a99374303a8d6d5
jazdzewt <jazdzewskitymon@gmail.com>
2026-01-12 17:23:57 +0100

    Zmiana przechowywania sekretów Flask.

--- web/app.py
+++ web/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import os
+
+def get_secret(secret_name, default=None):
+    # 1. Najpierw próbujemy Docker Secrets (plik)
+    try:
+        with open(f'/run/secrets/{secret_name}', 'r') as file:
+            return file.read().strip()
+    except IOError:
+        # 2. Jak nie ma pliku, szukamy w zmiennych (dla kompatybilności)
+        return os.environ.get(secret_name.upper(), default)
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = get_secret('flask_key_file', 'klucz_zapasowy_dev')
 
 @app.route('/')
 def hello():
