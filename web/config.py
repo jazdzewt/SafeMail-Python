7a65d58a448a9816de6f64909b2c2e9eb54f936b
jazdzewt <jazdzewskitymon@gmail.com>
2026-01-15 09:00:46 +0100

    Zmiana uzytkownika kontenera z aplikacja i zmiana lokalizacji sekretu Flask.

--- web/config.py
+++ web/config.py
@@ -1,5 +1,6 @@
 import os
 from utils import get_secret
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = get_secret('flask_key_file')
@@ -8,3 +9,4 @@ class Config:
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     WTF_CSRF_TIME_LIMIT = 3600
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
